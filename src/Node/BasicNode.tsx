3c2824958b5d1eeaa846f3a8a8c9b116e934f2f8
Robert Duke <robduke123@gmail.com>
2025-06-19 23:36:48 +1200

    created first pages

--- src/Node/BasicNode.tsx
+++ src/Node/BasicNode.tsx
@@ -51,7 +51,7 @@ const BasicNode = ({
 		updateFocusedIndex(index);
 	};
 
-	const onKeyDown: KeyboardEventHandler<HTMLDivElement> = (even) => {
+	const onKeyDown: KeyboardEventHandler<HTMLDivElement> = (event) => {
 		const { target } = event;
 	};
 
