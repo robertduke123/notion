49b8ad5b98b44ca79a8151b503be888c83c40fcc
Robert Duke <robduke123@gmail.com>
2025-07-05 17:15:04 +1200

    added dragable reordering

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,32 +1,49 @@
 import useFocusedNodeIndex from "./useFocusedNodeIndex";
 import { Cover } from "./Cover";
 import { Title } from "./Title";
-import { NodeTypeSwitcher } from "../Node/NodeTypeSwitcher";
+import { NodeContainer } from "../Node/NodeContainer";
 import Spacer from "./Spacer";
 import { nanoid } from "nanoid";
 import "./Page.module.css";
 import { useAppState } from "../state/AppStateContext";
+import { DndContext, DragOverlay, type DragEndEvent } from "@dnd-kit/core";
+import {
+	SortableContext,
+	verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 export const Page = () => {
-	const { nodes, title, setTitle, addNode } = useAppState();
+	const { nodes, title, setTitle, addNode, reorderNodes } = useAppState();
 	const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({
 		nodes,
 	});
+
+	const handleDragEvent = (event: DragEndEvent) => {
+		const { active, over } = event;
+		if (over?.id && over?.id !== active?.id) {
+			reorderNodes(active.id as string, over.id as string);
+		}
+	};
 
 	return (
 		<>
 			<Cover />
 			<div>
 				<Title addNode={addNode} title={title} changePageTitle={setTitle} />
-				{nodes.map((node, index) => (
-					<NodeTypeSwitcher
-						key={node.id}
-						node={node}
-						isFocused={focusedNodeIndex === index}
-						updateFocusedIndex={setFocusedNodeIndex}
-						index={index}
-					/>
-				))}
+				<DndContext onDragEnd={handleDragEvent}>
+					<SortableContext items={nodes} strategy={verticalListSortingStrategy}>
+						{nodes.map((node, index) => (
+							<NodeContainer
+								key={node.id}
+								node={node}
+								isFocused={focusedNodeIndex === index}
+								updateFocusedIndex={setFocusedNodeIndex}
+								index={index}
+							/>
+						))}
+					</SortableContext>
+					<DragOverlay />
+				</DndContext>
 				<Spacer
 					handleClick={() => {
 						addNode({ type: "text", value: "", id: nanoid() }, nodes.length);
