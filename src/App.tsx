38ebe899fc4150c06d4a8d5af88e5ac6d06531ff
Robert Duke <robduke123@gmail.com>
2025-07-09 23:10:58 +1200

    authentication with supbase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+import { Route, Routes } from "react-router-dom";
 import { Page } from "./Page/Page";
 import { AppStateProvider } from "./state/AppStateContext";
 import { createPage } from "./utils/createPage";
+const initialState = createPage();
+
+const Auth = () => {
+	return <div>Test</div>;
+};
 
 function App() {
-	const initialState = createPage();
-
 	return (
-		<AppStateProvider initialState={initialState}>
-			<Page />
-		</AppStateProvider>
+		<Routes>
+			<Route path="auth" element={<Auth />} />
+			<Route
+				path="/:id"
+				element={
+					<AppStateProvider initialState={initialState}>
+						<Page />
+					</AppStateProvider>
+				}
+			/>
+			<Route
+				path="/"
+				element={
+					<AppStateProvider initialState={initialState}>
+						<Page />
+					</AppStateProvider>
+				}
+			/>
+		</Routes>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { AuthSessionProvider } from "./auth/AuthSessionContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<BrowserRouter>
+			<AuthSessionProvider>
+				<App />
+			</AuthSessionProvider>
+		</BrowserRouter>
+	</StrictMode>
+);
